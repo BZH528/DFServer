89762b1988cf31a1709c146e629d3e510f5a4207
hulinfa <1225488947@qq.com>
2020-08-27 17:59:23 +0800

    删除不必要的变量文件

--- src/main/java/com/runone/var/ESconst.java
+++
@@ -1,7 +0,0 @@
-package com.runone.var;
-
-public class ESconst {
-
-    public  static String newsInfo = "com.runone.bean.SimpleNewsInfo";
-    public  static String notice = "com.runone.bean.GovernmentNoticeInfo";
-}
